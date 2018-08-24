3937e7ea096a936ced736d3271a27cd887f52bab
Japleen <japleen.kaur@wipro.com>
2018-08-25 02:42:28 +0530

    UI files for login and authentication

---
+++ app-services/authentication.service.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('AuthenticationService', AuthenticationService);
+
+    AuthenticationService.$inject = ['$http', 'UserService'];
+    function AuthenticationService($http, UserService) {
+        var service = {};
+
+        service.Login = Login;
+        //service.SetCredentials = SetCredentials;
+
+        return service;
+
+        function Login(username, password, callback) {
+
+
+            var authdata = window.btoa(username + ':' + password);
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
+            $http.post('http://localhost:8080/authenticate')
+               .then(function (response) {
+                   callback(response);
+               });
+
+        }
+    }
+
+
+
+})();
